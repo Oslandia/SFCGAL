2333575ee081eac5c5f6d25168f61e014f2a26ef
MBorne <mickael.borne@ign.fr>
2012-10-12 18:40:46 +0200

    [Coordinate]replace _x,_y,_z by _xyz:detail::CoordinateStorage relying on Epeck

---
+++ test/unit/SFCGAL/KernelTest.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+#include <boost/test/unit_test.hpp>
+
+#include <SFCGAL/Kernel.h>
+#include <SFCGAL/Coordinate.h>
+#include <SFCGAL/LineString.h>
+
+using namespace SFCGAL ;
+
+typedef Kernel::Vector_2          Vector_2 ;
+typedef Kernel::Vector_3          Vector_3 ;
+typedef Kernel::Point_2           Point_2 ;
+typedef Kernel::Point_3           Point_3 ;
+typedef Kernel::Segment_2         Segment_2 ;
+typedef Kernel::Segment_3         Segment_3 ;
+
+// always after CGAL
+using namespace boost::unit_test ;
+
+BOOST_AUTO_TEST_SUITE( SFCGAL_KernelTest )
+
+
+/**
+ * 3 lines intersecting on POINT(1/3 1)
+ */
+BOOST_AUTO_TEST_CASE( testIntersectsRobutness )
+{
+	LineString ab( Point(0.0,0.0),  Point(1.0,3.0) );
+	LineString cd( Point(0.0,1.0) , Point(1.0,1.0) );
+	LineString ef( Point(-1.0,3.0), Point(1.0,0.0) );
+
+	//ab, cd
+	CGAL::Object abIcd_ = CGAL::intersection(
+		Segment_2( ab.startPoint().toPoint_2< Kernel >(), ab.endPoint().toPoint_2< Kernel >() ),
+		Segment_2( cd.startPoint().toPoint_2< Kernel >(), cd.endPoint().toPoint_2< Kernel >() )
+	);
+	const Point_2 * abIcd = CGAL::object_cast<Point_2>(&abIcd_) ;
+	BOOST_REQUIRE( abIcd != NULL );
+
+	//would break robustness if construction history is lost
+	Point intersectionA( *abIcd );
+
+	CGAL::Object abIef_ = CGAL::intersection(
+		intersectionA.toPoint_2< Kernel >(),
+		Segment_2( ef.startPoint().toPoint_2< Kernel >(), ef.endPoint().toPoint_2< Kernel >() )
+	);
+	const Point_2 * abIef = CGAL::object_cast<Point_2>(&abIef_) ;
+	BOOST_REQUIRE( abIef != NULL );
+
+	Point intersectionB( *abIef );
+
+	BOOST_CHECK_EQUAL( intersectionA.toPoint_2< Kernel >(), intersectionB.toPoint_2< Kernel >() );
+}
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
---
+++ test/unit/SFCGAL/detail/CoordinateStorageTest.cpp
@@ -0,0 +1,46 @@
+#include <boost/test/unit_test.hpp>
+
+#include <SFCGAL/Kernel.h>
+#include <SFCGAL/numeric.h>
+#include <SFCGAL/detail/CoordinateStorage.h>
+
+
+using namespace SFCGAL ;
+
+// always after CGAL
+using namespace boost::unit_test ;
+
+
+BOOST_AUTO_TEST_SUITE( SFCGAL_detail_CoordinateStorageTest )
+
+
+BOOST_AUTO_TEST_CASE( testEmpty )
+{
+	detail::CoordinateStorage empty ;
+	BOOST_CHECK_EQUAL( empty.which(), 0 );
+	BOOST_CHECK( isNaN( detail::x( empty ) ) );
+	BOOST_CHECK( isNaN( detail::y( empty ) ) );
+	BOOST_CHECK( isNaN( detail::z( empty ) ) );
+}
+
+BOOST_AUTO_TEST_CASE( test2D )
+{
+	detail::CoordinateStorage empty( Kernel::Point_2(3.0,4.0) ) ;
+	BOOST_CHECK_EQUAL( empty.which(), 1 );
+	BOOST_CHECK_EQUAL( detail::x( empty ), 3.0 );
+	BOOST_CHECK_EQUAL( detail::y( empty ), 4.0 );
+	BOOST_CHECK( isNaN( detail::z( empty ) ) );
+}
+
+BOOST_AUTO_TEST_CASE( test3D )
+{
+	detail::CoordinateStorage empty( Kernel::Point_3(3.0,4.0,5.0) ) ;
+	BOOST_CHECK_EQUAL( empty.which(), 2 );
+	BOOST_CHECK_EQUAL( detail::x( empty ), 3.0 );
+	BOOST_CHECK_EQUAL( detail::y( empty ), 4.0 );
+	BOOST_CHECK_EQUAL( detail::z( empty ), 5.0 );
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
