cb8b9063e4e13ec770f70753b11c2a7f45ae0dee
mborne <mickael.borne@ign.fr>
2013-01-14 17:14:49 +0100

    merge bench

---
+++ test/bench/BenchWKT.cpp
@@ -0,0 +1,66 @@
+#include <fstream>
+
+#include <sys/time.h>
+
+#include <SFCGAL/all.h>
+#include <SFCGAL/io/wkt.h>
+
+#include "../test_config.h"
+
+#include <boost/timer/timer.hpp>
+#include <boost/test/unit_test.hpp>
+#include <boost/format.hpp>
+
+using namespace boost::unit_test ;
+using namespace SFCGAL ;
+
+#define N_WKTS 100000
+
+BOOST_AUTO_TEST_SUITE( SFCGAL_BenchWKT )
+
+//
+// Test limit case
+BOOST_AUTO_TEST_CASE( testReadPoints )
+{
+	boost::timer::cpu_timer timer;
+	timer.start();
+	for ( int i = 0; i < N_WKTS; i++ ){
+		io::readWkt( "POINT(50000 50000)" ) ;
+	}
+	timer.stop();
+	std::cout << N_WKTS << " WKT points : " << timer.format() << std::endl;
+}
+
+
+//
+// Test limit case
+BOOST_AUTO_TEST_CASE( testReadLineString )
+{
+	boost::timer::cpu_timer timer;
+	timer.start();
+	for ( int i = 0; i < N_WKTS; i++ ){
+		io::readWkt( "LINESTRING(0 0,0 1000,1000 1000)" ) ;
+	}
+	timer.stop();
+	std::cout << N_WKTS << " WKT LineStrings : " << timer.format() << std::endl;
+}
+
+//
+// Test limit case
+BOOST_AUTO_TEST_CASE( testReadPolygon )
+{
+	boost::timer::cpu_timer timer;
+	timer.start();
+	for ( int i = 0; i < N_WKTS; i++ ){
+		io::readWkt( "POLYGON((0 0,0 1000,1000 1000,1000 0,0 0))" ) ;
+	}
+	timer.stop();
+	std::cout << N_WKTS << " WKT Polygons : " << timer.format() << std::endl;
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+
+
