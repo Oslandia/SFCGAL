e7a086fc9915f16edccb23c000c07e75e8ddf6d2
mborne <mickael.borne@ign.fr>
2013-01-15 17:28:27 +0100

    add bench for wkt and avoid copies in reader

--- test/bench/BenchWKT.cpp
+++ test/bench/BenchWKT.cpp
@@ -58,6 +58,34 @@ BOOST_AUTO_TEST_CASE( testReadPolygon )
 	std::cout << N_WKTS << " WKT Polygons : " << timer.format() << std::endl;
 }
 
+//
+// Test limit case
+BOOST_AUTO_TEST_CASE( testReadTriangle )
+{
+	boost::timer::cpu_timer timer;
+	timer.start();
+	for ( int i = 0; i < N_WKTS; i++ ){
+		io::readWkt( "TRIANGLE((0 0,0 1000,1000 1000,1000 0,0 0))" ) ;
+	}
+	timer.stop();
+	std::cout << N_WKTS << " WKT Triangles : " << timer.format() << std::endl;
+}
+
+//
+// Test limit case
+BOOST_AUTO_TEST_CASE( testReadSolid )
+{
+	boost::timer::cpu_timer timer;
+	timer.start();
+	for ( int i = 0; i < N_WKTS; i++ ){
+		io::readWkt( "SOLID((((0.0 0.0 0.0,0.0 1.0 0.0,1.0 1.0 0.0,1.0 0.0 0.0,0.0 0.0 0.0),(0.2 0.2 0.0,0.8 0.2 0.0,0.8 0.8 0.0,0.2 0.8 0.0,0.2 0.2 0.0)),((0.0 0.0 1.0,1.0 0.0 1.0,1.0 1.0 1.0,0.0 1.0 1.0,0.0 0.0 1.0),(0.2 0.2 1.0,0.2 0.8 1.0,0.8 0.8 1.0,0.8 0.2 1.0,0.2 0.2 1.0)),((0.0 0.0 0.0,0.0 0.0 1.0,0.0 1.0 1.0,0.0 1.0 0.0,0.0 0.0 0.0)),((0.0 1.0 0.0,0.0 1.0 1.0,1.0 1.0 1.0,1.0 1.0 0.0,0.0 1.0 0.0)),((1.0 1.0 0.0,1.0 1.0 1.0,1.0 0.0 1.0,1.0 0.0 0.0,1.0 1.0 0.0)),((1.0 0.0 0.0,1.0 0.0 1.0,0.0 0.0 1.0,0.0 0.0 0.0,1.0 0.0 0.0)),((0.2 0.2 0.0,0.2 0.2 1.0,0.8 0.2 1.0,0.8 0.2 0.0,0.2 0.2 0.0)),((0.8 0.2 0.0,0.8 0.2 1.0,0.8 0.8 1.0,0.8 0.8 0.0,0.8 0.2 0.0)),((0.8 0.8 0.0,0.8 0.8 1.0,0.2 0.8 1.0,0.2 0.8 0.0,0.8 0.8 0.0)),((0.2 0.8 0.0,0.2 0.8 1.0,0.2 0.2 1.0,0.2 0.2 0.0,0.2 0.8 0.0))))" ) ;
+	}
+	timer.stop();
+	std::cout << N_WKTS << " WKT Solids : " << timer.format() << std::endl;
+}
+
+
+
 
 BOOST_AUTO_TEST_SUITE_END()
 
