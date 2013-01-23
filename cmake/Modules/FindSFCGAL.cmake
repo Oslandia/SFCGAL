fcff93cd2a8cbeb167be7edf68488fb1ba66ee66
mborne <mickael.borne@ign.fr>
2013-01-23 11:04:35 +0100

    finder cmake

--- cmake/Modules/FindSFCGAL.cmake
+++ cmake/Modules/FindSFCGAL.cmake
@@ -22,8 +22,8 @@ IF(SFCGAL_INCLUDE_DIR)
 	IF(EXISTS "${SFCGAL_INCLUDE_DIR}/SFCGAL/version.h")
 		FILE(STRINGS "${SFCGAL_INCLUDE_DIR}/SFCGAL/version.h" sfcgal_version_str REGEX "^#define[\t ]+SFCGAL_VERSION[\t ]+\".*\"")
 
-		STRING(REGEX REPLACE "^#define[\t ]+SFCGAL_VERSION[\t ]+\"([^\"]*)\".*" "\\1" SFCGAL_VERSION "${ign_socle_version_str}")
-
+		STRING(REGEX REPLACE "^#define[\t ]+SFCGAL_VERSION[\t ]+\"([^\"]*)\".*" "\\1" SFCGAL_VERSION "${sfcgal_version_str}")
+		
 		# from http://www.cmake.org/Wiki/CMakeCompareVersionStrings 
 		# Breaks up a string in the form n1.n2.n3 into three parts and stores
 		# them in major, minor, and patch.  version should be a value, not a
@@ -33,7 +33,7 @@ IF(SFCGAL_INCLUDE_DIR)
 			STRING(REGEX REPLACE "^[0-9]+\\.([0-9])+\\.[0-9]+" "\\1" SFCGAL_MINOR_VERSION "${SFCGAL_VERSION}")
 			STRING(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+)" "\\1" SFCGAL_PATCH_VERSION "${SFCGAL_VERSION}")
 		ELSE()
-			MESSAGE( WARNING "IGN_SOCLE_VERSION (${SFCGAL_VERSION}) doesn't match *.*.* form" )
+			MESSAGE( WARNING "SFCGAL_VERSION (${SFCGAL_VERSION}) doesn't match *.*.* form" )
 		ENDIF()		
 
 
@@ -59,7 +59,7 @@ find_library(SFCGAL_LIBRARY_DEBUG NAMES SFCGALd
 # set output variables
 set( SFCGAL_INCLUDE_DIRS "${SFCGAL_INCLUDE_DIR}" )
 if ( SFCGAL_LIBRARY_DEBUG )
-	set( SFCGAL_LIBRARIES "optimized;${SFCGAL_LIBRARY};debug;${SFCGAL_LIBRARY_DEBUG} )
+	set( SFCGAL_LIBRARIES "optimized;${SFCGAL_LIBRARY};debug;${SFCGAL_LIBRARY_DEBUG}" )
 else()
 	set( SFCGAL_LIBRARIES ${SFCGAL_LIBRARY} )
 endif()
