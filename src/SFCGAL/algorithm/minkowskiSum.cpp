7ba8971eee5204859f1c362332c71a26602b957d
mborne <mickael.borne@ign.fr>
2013-01-24 14:01:07 +0100

    [MinkowskiSum]holes for polygon

--- src/SFCGAL/algorithm/minkowskiSum.cpp
+++ src/SFCGAL/algorithm/minkowskiSum.cpp
@@ -48,6 +48,28 @@ std::auto_ptr< Geometry > minkowskiSum( const Point& gA, const Polygon& gB )
 	return std::auto_ptr< Geometry >( gB.clone() ) ;
 }
 
+
+/*
+ * append gA+gB into the polygonSet
+ */
+void minkowskiSum( const LineString& gA, const Polygon_2 & gB, Polygon_set_2 & polygonSet ){
+	int npt = gA.numPoints() ;
+	for ( int i = 0; i < npt - 1 ; i++ ){
+		Polygon_2 P;
+		P.push_back( gA.pointN(i).toPoint_2() );
+		P.push_back( gA.pointN(i+1).toPoint_2() );
+
+		Polygon_with_holes_2 part = minkowski_sum_2( P, gB );
+
+		// merge into a polygon set
+		if ( polygonSet.is_empty() ){
+			polygonSet.insert( part );
+		}else{
+			polygonSet.join( part );
+		}
+	}
+}
+
 ///
 ///
 ///
@@ -59,37 +81,14 @@ std::auto_ptr< Geometry > minkowskiSum( const LineString& gA, const Polygon& gB 
 
 	Polygon_2 Q = gB.toPolygon_2() ;
 
-	Polygon_set_2 polygonSet ;
-
 	// compute minkowski sum for each LineString (as a collapsed polygon), and join the results
+	Polygon_set_2 polygonSet ;
 	{
-		int npt = gA.numPoints() ;
-		for ( int i = 0; i < npt - 1 ; i++ ){
-			Polygon_2 P;
-			P.push_back( gA.pointN(i).toPoint_2() );
-			P.push_back( gA.pointN(i+1).toPoint_2() );
-
-			Polygon_with_holes_2 part = minkowski_sum_2( P, Q );
-			// if linestring has only one segment
-			if ( npt == 2 ){
-				return std::auto_ptr< Geometry >(
-					new Polygon( part )
-				);
-			}
-
-			// merge into a polygon set
-			if ( i == 0 ){
-				polygonSet.insert( part );
-			}else{
-				polygonSet.join( part );
-			}
-		}
+		minkowskiSum( gA, Q, polygonSet );
 	}
-
 	std::auto_ptr< MultiPolygon > result( detail::polygonSetToMultiPolygon( polygonSet ) );
 	return std::auto_ptr< Geometry >( result.release() );
 }
-
 
 ///
 ///
@@ -111,15 +110,39 @@ std::auto_ptr< Geometry > minkowskiSum( const Polygon& gA, const Polygon& gB )
 	}
 
 	/*
-	 * Invoke minkowski_sum_2 for exterior ring and compute the difference with the
-	 * interiorRings of the results for each ring (if any)
+	 * Invoke minkowski_sum_2 for exterior ring
 	 */
 	Polygon_set_2 polygonSet ;
 	polygonSet.insert( minkowski_sum_2( gA.exteriorRing().toPolygon_2(), Q ) );
-//	for ( size_t i = 0; i < gA.numInteriorRings(); i++ ){
-//		Polygon_with_holes_2 holeSum = minkowski_sum_2( gA.interiorRingN(i).toPolygon_2(), Q ) ;
-//		polygonSet.difference( holeSum.outer_boundary() );
-//	}
+
+	/*
+	 * Compute the minkowski sum for each segment of the interior rings
+	 * and perform the union of the result.
+	 *
+	 * Interior rings will corresponds
+	 */
+	if ( gA.hasInteriorRings() ){
+		Polygon_set_2 sumInteriorRings ;
+		for ( size_t i = 0; i < gA.numInteriorRings(); i++ ){
+			minkowskiSum( gA.interiorRingN(i), Q, sumInteriorRings ) ;
+		}
+
+		/*
+		 * compute the difference for each holes of the resulting polygons
+		 */
+		std::list<Polygon_with_holes_2> interiorPolygons ;
+		sumInteriorRings.polygons_with_holes( std::back_inserter(interiorPolygons) ) ;
+		for ( std::list<Polygon_with_holes_2>::iterator it_p = interiorPolygons.begin();
+				it_p != interiorPolygons.end(); ++it_p ){
+
+			for ( Polygon_with_holes_2::Hole_iterator it_hole = it_p->holes_begin();
+					it_hole != it_p->holes_end(); ++it_hole ){
+
+				it_hole->reverse_orientation() ;
+				polygonSet.difference( *it_hole ) ;
+			} // foreach hole
+		}// foreach polygon
+	}
 
 	/*
 	 * convert the result into a SFCGAL::MultiPolygon
