46b6d70eff79c07c8f103bb29e2b54bc85dbc97a
mborne <mickael.borne@ign.fr>
2013-01-25 16:18:33 +0100

    [Length]comments

--- include/SFCGAL/algorithm/length.h
+++ include/SFCGAL/algorithm/length.h
@@ -12,30 +12,32 @@ namespace SFCGAL {
 namespace algorithm {
 
 	/**
-	 * @brief Get the 2D length for a LineString
+	 * @brief Compute the 2D length for a Geometry (0 for incompatible types)
+	 */
+	double length( const Geometry & g ) ;
+	/**
+	 * @brief Compute the 2D length for a LineString
 	 */
 	double length( const LineString & g ) ;
 	/**
-	 * @brief Get the 2D length for a GeometryCollection
+	 * @brief Compute the 2D length for a GeometryCollection
 	 */
 	double length( const GeometryCollection & g ) ;
-	/**
-	 * @brief Get the 2D length for a geometry
-	 */
-	double length( const Geometry & g ) ;
 
 	/**
-	 * @brief Get the 3D length for a LineString
+	 * @brief Compute the 2D length for a geometry
+	 * @return the length of the Geometry, 0 for incompatible types
+	 */
+	double length3D( const Geometry & g ) ;
+	/**
+	 * @brief Compute the 3D length for a LineString
 	 */
 	double length3D( const LineString & g ) ;
 	/**
-	 * @brief Get the 3D length for a GeometryCollection
+	 * @brief Compute the 3D length for a GeometryCollection
 	 */
 	double length3D( const GeometryCollection & g ) ;
-	/**
-	 * @brief Get the 2D length for a geometry
-	 */
-	double length3D( const Geometry & g ) ;
+
 
 }//algorithm
 }//SFCGAL
