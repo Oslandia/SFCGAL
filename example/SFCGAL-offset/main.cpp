08c80d25ff990da234e7e4a0381142f5d823ba59
mborne <mickael.borne@ign.fr>
2013-01-29 17:49:31 +0100

    add offset example

---
+++ example/SFCGAL-offset/main.cpp
@@ -0,0 +1,17 @@
+#include <SFCGAL/all.h>
+
+#include <SFCGAL/io/wkt.h>
+#include <SFCGAL/algorithm/offset.h>
+
+using namespace SFCGAL ;
+
+int main( int argc, char* argv[] ){
+	std::auto_ptr< Geometry > g( io::readWkt("MULTIPOINT(0 0,5 6,3 2,7 1,4 1,3 5,2 9)") );
+
+	for ( size_t i = 1; i <= 50; i++ ){
+		std::auto_ptr< Geometry > buffer( algorithm::offset( *g, 0.2*i ) );
+		std::cout << buffer->asText(5) << std::endl ;
+	}
+
+	return 0 ;
+}
