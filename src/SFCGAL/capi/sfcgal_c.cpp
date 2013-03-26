8d399f80947bdb9e0d2d0bf7a64f4dcf4ad105e0
Hugo Mercier <hugo.mercier@oslandia.com>
2013-03-26 15:29:08 -0400

    Fix C API for tesselate

--- src/SFCGAL/capi/sfcgal_c.cpp
+++ src/SFCGAL/capi/sfcgal_c.cpp
@@ -47,7 +47,7 @@
 #include <SFCGAL/algorithm/plane.h>
 #include <SFCGAL/algorithm/area.h>
 #include <SFCGAL/algorithm/extrude.h>
-#include <SFCGAL/triangulate/triangulatePolygon.h>
+#include <SFCGAL/algorithm/tesselate.h>
 #include <SFCGAL/triangulate/triangulate2DZ.h>
 #include <SFCGAL/algorithm/minkowskiSum.h>
 #include <SFCGAL/algorithm/offset.h>
@@ -568,6 +568,7 @@ SFCGAL_GEOMETRY_FUNCTION_BINARY_CONSTRUCTION( intersection_3d, SFCGAL::algorithm
 SFCGAL_GEOMETRY_FUNCTION_UNARY_CONSTRUCTION( convexhull, SFCGAL::algorithm::convexHull )
 SFCGAL_GEOMETRY_FUNCTION_UNARY_CONSTRUCTION( convexhull_3d, SFCGAL::algorithm::convexHull3D )
 SFCGAL_GEOMETRY_FUNCTION_UNARY_CONSTRUCTION( straight_skeleton, SFCGAL::algorithm::straightSkeleton )
+SFCGAL_GEOMETRY_FUNCTION_UNARY_CONSTRUCTION( tesselate, SFCGAL::algorithm::tesselate )
 
 #define SFCGAL_GEOMETRY_FUNCTION_UNARY_MEASURE( name, sfcgal_function ) \
 	extern "C" double sfcgal_geometry_##name( const sfcgal_geometry_t* ga ) \
@@ -664,24 +665,6 @@ extern "C" sfcgal_geometry_t* sfcgal_geometry_force_z_up( const sfcgal_geometry_
 		return 0;
 	}
 	return gb;
-}
-
-extern "C" sfcgal_geometry_t* sfcgal_geometry_tesselate( const sfcgal_geometry_t* ga )
-{
-	const SFCGAL::Geometry* g = reinterpret_cast<const SFCGAL::Geometry*>(ga);
-	SFCGAL::TriangulatedSurface* surf = new SFCGAL::TriangulatedSurface;
-	try
-	{
-		SFCGAL::triangulate::triangulatePolygon3D( *g, *surf );
-	}
-	catch ( std::exception& e )
-	{
-		SFCGAL_WARNING( "During triangulate(A) :" );
-		SFCGAL_WARNING( "  with A: %s", ((const SFCGAL::Geometry*)(ga))->asText().c_str() );
-		SFCGAL_ERROR( "%s", e.what() );
-		return 0;
-	}
-	return static_cast<SFCGAL::Geometry*>(surf);
 }
 
 extern "C" sfcgal_geometry_t* sfcgal_geometry_triangulate_2dz( const sfcgal_geometry_t* ga )
