02d56072ffa49462746f4f3aa771eb7c8b367c34
mborne <mick25390@hotmail.fr>
2013-05-08 00:19:38 +0200

    fix crash for extrude with closed surface

--- test/unit/SFCGAL/algorithm/ExtrudeTest.cpp
+++ test/unit/SFCGAL/algorithm/ExtrudeTest.cpp
@@ -147,6 +147,14 @@ BOOST_AUTO_TEST_CASE( testExtrudeSquareWithHole )
 }
 
 
+// test with a closed surface
+BOOST_AUTO_TEST_CASE( testExtrudePolyhedralSurface_closed )
+{
+	std::auto_ptr< Geometry > g( io::readWkt("POLYHEDRALSURFACE(((0 0 0, 0 0 1, 0 1 1, 0 1 0, 0 0 0)),((0 0 0, 0 1 0, 1 1 0, 1 0 0, 0 0 0)), ((0 0 0, 1 0 0, 1 0 1, 0 0 1, 0 0 0)), ((1 1 0, 1 1 1, 1 0 1, 1 0 0, 1 1 0)),((0 1 0, 0 1 1, 1 1 1, 1 1 0, 0 1 0)),((0 0 1, 1 0 1, 1 1 1, 0 1 1, 0 0 1)))") );
+	std::auto_ptr< Geometry > ext( algorithm::extrude( *g, 0.0, 0.0, 1.0 ) );
+}
+
+
 
 BOOST_AUTO_TEST_SUITE_END()
 
