d4469776369ab3f58951de505c4f6db05a0e5948
mborne <mickael.borne@ign.fr>
2013-05-15 18:06:17 +0200

    [TriangulatedSurface]add tests

--- test/unit/SFCGAL/TriangulatedSurfaceTest.cpp
+++ test/unit/SFCGAL/TriangulatedSurfaceTest.cpp
@@ -19,6 +19,7 @@
  *
  */
 #include <SFCGAL/Kernel.h>
+#include <SFCGAL/Envelope.h>
 #include <SFCGAL/Triangle.h>
 #include <SFCGAL/PolyhedralSurface.h>
 #include <SFCGAL/TriangulatedSurface.h>
@@ -30,7 +31,171 @@ using namespace boost::unit_test ;
 
 using namespace SFCGAL ;
 
-BOOST_AUTO_TEST_SUITE( SFCGAL_TINTest )
+BOOST_AUTO_TEST_SUITE( SFCGAL_TriangulatedSurfaceTest )
+
+//TriangulatedSurface() ;
+BOOST_AUTO_TEST_CASE( defaultConstructor )
+{
+	TriangulatedSurface g ;
+	BOOST_CHECK( g.isEmpty() ) ;
+	BOOST_CHECK_EQUAL( g.numTriangles(), 0U ) ;
+}
+//TriangulatedSurface( const std::vector< Triangle > & triangle ) ;
+BOOST_AUTO_TEST_CASE( constructorWithTriangles )
+{
+	std::vector< Triangle > triangles ;
+	triangles.push_back( Triangle( Point(0.0,0.0), Point(1.0,0.0), Point(1.0,1.0) ) ) ;
+	triangles.push_back( Triangle( Point(0.0,0.0), Point(1.0,1.0), Point(0.0,1.0) ) ) ;
+
+	TriangulatedSurface g( triangles ) ;
+	BOOST_CHECK( ! g.isEmpty() ) ;
+	BOOST_CHECK_EQUAL( g.numTriangles(), 2U ) ;
+}
+
+//TriangulatedSurface( TriangulatedSurface const& other ) ;
+//TriangulatedSurface& operator = ( const TriangulatedSurface & other ) ;
+//~TriangulatedSurface() ;
+
+//inline size_t             numTriangles() const { return _triangles.size(); }
+//inline const Triangle  &  triangleN( size_t const& n ) const {
+//inline Triangle &         triangleN( size_t const& n ) {
+//inline void               addTriangle( const Triangle & triangle )
+//inline void               addTriangle( Triangle * triangle )
+//void                      addTriangles( const TriangulatedSurface & other ) ;
+
+
+//virtual size_t              numGeometries() const ;
+//virtual const Triangle  &   geometryN( size_t const& n ) const ;
+//virtual Triangle &          geometryN( size_t const& n ) ;
+
+
+//void reserve( const size_t & n ) ;
+
+//-- iterators
+
+//inline iterator       begin() {
+//inline const_iterator begin() const {
+//inline iterator       end() {
+//inline const_iterator end() const {
+
+
+
+//-- helpers
+
+//template < typename K, typename Polyhedron > std::auto_ptr<Polyhedron> toPolyhedron_3() const;
+// TODO
+
+
+//-- Geometry tests
+
+//virtual Geometry *   Geometry::clone() const = 0 ;
+BOOST_AUTO_TEST_CASE( testClone )
+{
+	std::vector< Triangle > triangles ;
+	triangles.push_back( Triangle( Point(0.0,0.0), Point(1.0,0.0), Point(1.0,1.0) ) ) ;
+	triangles.push_back( Triangle( Point(0.0,0.0), Point(1.0,1.0), Point(0.0,1.0) ) ) ;
+
+	TriangulatedSurface g( triangles ) ;
+
+	std::auto_ptr< Geometry > copy( g.clone() );
+	BOOST_REQUIRE( copy->is< TriangulatedSurface >() );
+	BOOST_CHECK_EQUAL( copy->as< TriangulatedSurface >().numTriangles(), 2U );
+}
+
+//virtual Geometry*    Geometry::boundary() const ;
+BOOST_AUTO_TEST_CASE( testBoundary )
+{
+	std::vector< Triangle > triangles ;
+	triangles.push_back( Triangle( Point(0.0,0.0), Point(1.0,0.0), Point(1.0,1.0) ) ) ;
+	triangles.push_back( Triangle( Point(0.0,0.0), Point(1.0,1.0), Point(0.0,1.0) ) ) ;
+
+	TriangulatedSurface g( triangles ) ;
+	std::auto_ptr< Geometry > boundary( g.boundary() );
+	// TODO add algorithm::lineMerge and update
+	BOOST_CHECK_EQUAL( boundary->asText(0), "MULTILINESTRING((0 0,1 0),(1 0,1 1),(1 1,0 1),(0 1,0 0))" );
+}
+BOOST_AUTO_TEST_CASE( testBoundaryClosed )
+{
+	Point a( 0.0, 0.0, 0.0 );
+	Point b( 1.0, 0.0, 0.0 );
+	Point c( 0.0, 1.0, 0.0 );
+	Point d( 0.0, 0.0, 1.0 );
+
+	std::vector< Triangle > triangles ;
+	triangles.push_back( Triangle( a, c, b ) ) ;
+	triangles.push_back( Triangle( a, b, d ) ) ;
+	triangles.push_back( Triangle( b, c, d ) ) ;
+	triangles.push_back( Triangle( c, a, d ) ) ;
+
+	TriangulatedSurface g( triangles ) ;
+	std::auto_ptr< Geometry > boundary( g.boundary() );
+	BOOST_CHECK( boundary->isEmpty() );
+}
+
+
+//Envelope             Geometry::envelope() const ;
+BOOST_AUTO_TEST_CASE( testEnvelope )
+{
+	Point a( 0.0, 0.0, 0.0 );
+	Point b( 1.0, 0.0, 0.0 );
+	Point c( 0.0, 1.0, 0.0 );
+	Point d( 0.0, 0.0, 1.0 );
+
+	std::vector< Triangle > triangles ;
+	triangles.push_back( Triangle( a, c, b ) ) ;
+	triangles.push_back( Triangle( a, b, d ) ) ;
+	triangles.push_back( Triangle( b, c, d ) ) ;
+	triangles.push_back( Triangle( c, a, d ) ) ;
+
+	TriangulatedSurface g( triangles ) ;
+	Envelope bbox = g.envelope() ;
+	BOOST_CHECK_EQUAL( bbox.xMin(), 0.0 );
+	BOOST_CHECK_EQUAL( bbox.xMax(), 1.0 );
+	BOOST_CHECK_EQUAL( bbox.yMin(), 0.0 );
+	BOOST_CHECK_EQUAL( bbox.yMax(), 1.0 );
+	BOOST_CHECK_EQUAL( bbox.zMin(), 0.0 );
+	BOOST_CHECK_EQUAL( bbox.zMax(), 1.0 );
+}
+//std::string          Geometry::asText( const int & numDecimals = -1 ) const ;
+//TODO
+
+//virtual std::string  Geometry::geometryType() const = 0 ;
+BOOST_AUTO_TEST_CASE( testGeometryType )
+{
+	TriangulatedSurface g;
+	BOOST_CHECK_EQUAL( g.geometryType(), "TriangulatedSurface" );
+}
+//virtual GeometryType Geometry::geometryTypeId() const = 0 ;
+BOOST_AUTO_TEST_CASE( testGeometryTypeId )
+{
+	TriangulatedSurface g;
+	BOOST_CHECK_EQUAL( g.geometryTypeId(), TYPE_TRIANGULATEDSURFACE );
+}
+
+//virtual int          Geometry::dimension() const = 0 ;
+BOOST_AUTO_TEST_CASE( testDimension )
+{
+	TriangulatedSurface g;
+	BOOST_CHECK_EQUAL( g.dimension(), 2 ); //surface
+}
+
+//virtual int          Geometry::coordinateDimension() const = 0 ;
+//virtual bool         Geometry::isEmpty() const = 0 ;
+//virtual bool         Geometry::is3D() const = 0 ;
+//virtual bool         Geometry::isMeasured() const = 0 ;
+//virtual bool         Geometry::isSimple() const = 0 ;
+//template < typename Derived > inline bool Geometry::is() const
+BOOST_AUTO_TEST_CASE( isTriangulatedSurface )
+{
+	TriangulatedSurface g;
+	BOOST_CHECK( g.is< TriangulatedSurface >() );
+}
+//template < typename Derived > inline const Derived &  Geometry::as() const
+//template < typename Derived > inline Derived &        Geometry::as()
+
+
+
+//-- other tests
 
 BOOST_AUTO_TEST_CASE( polyhedronConversionTest )
 {
