6a7fafdea9bb606aa8fa59ca141b760a9609e279
Hugo Mercier <hugo.mercier@oslandia.com>
2013-06-11 09:13:09 +0200

    Fix a namespace

--- src/SFCGAL/io/osg.cpp
+++ src/SFCGAL/io/osg.cpp
@@ -17,7 +17,7 @@ namespace io {
 ///
 void osgWriteFile( const Geometry & g, const std::string& filepath )
 {
-	detail::OsgFactory factory ;
+	SFCGAL::detail::io::OsgFactory factory ;
 	osg::ref_ptr< osg::Geometry > osgGeometry = factory.createGeometry( g );
 	osg::ref_ptr< osg::Geode > geode = new osg::Geode;
 	geode->setName( g.geometryType() );
