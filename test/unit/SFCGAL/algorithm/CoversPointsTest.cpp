9a41d498563cbd652bcb8fc37d4b7cee0a1086e8
Hugo Mercier <hugo.mercier@oslandia.com>
2013-06-20 15:19:36 +0200

    Minor : add (failing) test cases for covers

--- test/unit/SFCGAL/algorithm/CoversPointsTest.cpp
+++ test/unit/SFCGAL/algorithm/CoversPointsTest.cpp
@@ -31,7 +31,7 @@ using namespace SFCGAL ;
 using namespace SFCGAL::detail ;
 using namespace boost::unit_test ;
 
-BOOST_AUTO_TEST_SUITE( SFCGAL_algorithm_CoversTest )
+BOOST_AUTO_TEST_SUITE( SFCGAL_algorithm_CoversPointsTest )
 
 BOOST_AUTO_TEST_CASE( testPointPointCoversPoints )
 {
@@ -64,6 +64,25 @@ BOOST_AUTO_TEST_CASE( testPolygonPolygonCoversPoints )
 		BOOST_CHECK_EQUAL( algorithm::coversPoints( *p1, *p2 ), true );
 		BOOST_CHECK_EQUAL( algorithm::coversPoints3D( *p1, *p2 ), true );
 	}
+}
+
+BOOST_AUTO_TEST_CASE( testCollectionCoversPoints )
+{
+#if 0
+	{
+		std::auto_ptr<Geometry> p1 = io::readWkt("GEOMETRYCOLLECTION(LINESTRING(0 0 0,0 0 1),LINESTRING(0 0 0,0 1 0),LINESTRING(0 1 1,0 0 1),LINESTRING(0 1 1,0 1 0))");
+		std::auto_ptr<Geometry> p2 = io::readWkt("TIN(((0 0.5 0.5,0 0 1,0 0 0,0 0.5 0.5)),((0 0 1,0 0.5 0.5,0 1 1,0 0 1)),((0 0.5 0.5,0 0 0,0 1 0,0 0.5 0.5)),((0 0.5 0.5,0 1 0,0 1 1,0 0.5 0.5)))");
+		std::cout << "p1 covers p2 ? " << algorithm::coversPoints3D( *p1, *p2 ) << std::endl;
+		std::cout << "p2 covers p1 ? " << algorithm::coversPoints3D( *p2, *p1 ) << std::endl;
+	}
+
+	{
+		std::auto_ptr<Geometry> p1 = io::readWkt("GEOMETRYCOLLECTION(TRIANGLE((1 1,0.5 0.5,0 1,1 1)),TRIANGLE((1 0,0.5 0.5,1 1,1 0)),TRIANGLE((0.5 0.5,0 0,0 1,0.5 0.5)))");
+		std::auto_ptr<Geometry> p2 = io::readWkt("TRIANGLE((1 0, 0 0,0.5 0.5,1 0))");
+		std::cout << "p1 covers p2 ? " << algorithm::coversPoints3D( *p1, *p2 ) << std::endl;
+		std::cout << "p2 covers p1 ? " << algorithm::coversPoints3D( *p2, *p1 ) << std::endl;
+	}
+#endif
 }
 
 BOOST_AUTO_TEST_SUITE_END()
