ca98cdfb26e0009e76a614a38caafd171ea56ef4
vmora <vincent.mora@oslandia.com>
2013-10-02 11:20:39 +0200

    changed licence to lgpl v2+

--- include/SFCGAL/algorithm/distance.h
+++ include/SFCGAL/algorithm/distance.h
@@ -4,20 +4,20 @@
  *   Copyright (C) 2012-2013 Oslandia <infos@oslandia.com>
  *   Copyright (C) 2012-2013 IGN (http://www.ign.fr)
  *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *   
+ *   This library is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef _SFCGAL_ALGORITHM_DISTANCE_H_
 #define _SFCGAL_ALGORITHM_DISTANCE_H_
 
--- include/SFCGAL/io/osg.h
+++ include/SFCGAL/io/osg.h
@@ -1,3 +1,23 @@
+/**
+ *   SFCGAL
+ *
+ *   Copyright (C) 2012-2013 Oslandia <infos@oslandia.com>
+ *   Copyright (C) 2012-2013 IGN (http://www.ign.fr)
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *   
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _SFCGAL_IO_OSG_H_
 #define _SFCGAL_IO_OSG_H_
 
@@ -23,11 +43,8 @@ namespace io {
 
 /**
  * @brief [helper] write an OSG geometry in a file. The format depends on the extension (http://www.openscenegraph.org/projects/osg/wiki/Support/UserGuides/Plugins)
- *
  * Note that osgviewer command line executable allows the visualization of the resulting files, screenshots and co
- *
  * @warning requires OpenSceneGraph
- *
  * @todo list of geometries?
  */
 SFCGAL_API void osgWriteFile( const Geometry& g, const std::string& filepath ) ;
