efd12447e974cd825b5042d63887f8d27f7a1178
vmora <vincent.mora@oslandia.com>
2014-10-21 18:20:00 +0200

    union first steps (work in progress)

---
+++ src/algorithm/union.cpp
@@ -0,0 +1,234 @@
+/**
+ *   SFCGAL
+ *
+ *   Copyright (C) 2012-2013 Oslandia <infos@oslandia.com>
+ *   Copyright (C) 2012-2013 IGN (http://www.ign.fr)
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *   
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <SFCGAL/algorithm/union.h>
+#include <SFCGAL/Exception.h>
+#include <SFCGAL/algorithm/intersects.h>
+#include <SFCGAL/algorithm/collect.h>
+#include <SFCGAL/algorithm/collectionHomogenize.h>
+#include <SFCGAL/detail/tools/Registry.h>
+#include <SFCGAL/detail/GeometrySet.h>
+#include <SFCGAL/algorithm/isValid.h>
+
+#include <CGAL/Boolean_set_operations_2.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/intersections.h>
+
+//
+// Union kernel
+
+using namespace SFCGAL::detail;
+
+namespace SFCGAL {
+
+typedef CGAL::Point_2<Kernel> Point_2;
+typedef CGAL::Segment_2<Kernel> Segment_2;
+typedef CGAL::Triangle_2<Kernel> Triangle_2;
+
+namespace algorithm {
+void union_( const PrimitiveHandle<3>& pa, const PrimitiveHandle<3>& pb,
+                   GeometrySet<3>& output, dim_t<3> ){BOOST_ASSERT(false);}
+// see Intersection2D.cpp
+void union_( const PrimitiveHandle<2>& pa, const PrimitiveHandle<2>& pb,
+                   GeometrySet<2>& output, dim_t<2> ){BOOST_ASSERT(false);}
+//
+// We deal here with symmetric call
+template <int Dim>
+void dispatch_union_sym( const PrimitiveHandle<Dim>& pa, const PrimitiveHandle<Dim>& pb,
+                                GeometrySet<Dim>& output )
+{
+    // assume types are ordered by dimension within the boost::variant
+    if ( pa.handle.which() >= pb.handle.which() ) {
+        union_( pa, pb, output, dim_t<Dim>() );
+    }
+    else {
+        union_( pb, pa, output, dim_t<Dim>() );
+    }
+}
+
+template <int Dim>
+void union_( const PrimitiveHandle<Dim>& pa, const PrimitiveHandle<Dim>& pb, GeometrySet<Dim>& output )
+{
+    dispatch_union_sym( pa, pb, output );
+}
+
+template void union_<2>( const PrimitiveHandle<2>& a, const PrimitiveHandle<2>& b, GeometrySet<2>& );
+template void union_<3>( const PrimitiveHandle<3>& a, const PrimitiveHandle<3>& b, GeometrySet<3>& );
+
+/**
+ * union post processing
+ */
+void post_union( const GeometrySet<2>& input, GeometrySet<2>& output )
+{
+    //
+    // reverse orientation of polygons if needed
+    for ( GeometrySet<2>::SurfaceCollection::const_iterator it = input.surfaces().begin();
+            it != input.surfaces().end();
+            ++it ) {
+        const CGAL::Polygon_with_holes_2<Kernel>& p = it->primitive();
+        CGAL::Polygon_2<Kernel> outer = p.outer_boundary();
+
+        if ( outer.orientation() == CGAL::CLOCKWISE ) {
+            outer.reverse_orientation();
+        }
+
+        std::list<CGAL::Polygon_2<Kernel> > rings;
+
+        for ( CGAL::Polygon_with_holes_2<Kernel>::Hole_const_iterator hit = p.holes_begin();
+                hit != p.holes_end();
+                ++hit ) {
+            rings.push_back( *hit );
+
+            if ( hit->orientation() == CGAL::COUNTERCLOCKWISE ) {
+                rings.back().reverse_orientation();
+            }
+        }
+
+        output.surfaces().push_back( CGAL::Polygon_with_holes_2<Kernel>( outer, rings.begin(), rings.end() ) );
+    }
+
+    output.points() = input.points();
+    output.segments() = input.segments();
+    output.volumes() = input.volumes();
+}
+
+void post_union( const GeometrySet<3>& input, GeometrySet<3>& output )
+{
+    // nothing special to do
+    output = input;
+}
+
+template <int Dim>
+void union_( const GeometrySet<Dim>& a, const GeometrySet<Dim>& b, GeometrySet<Dim>& output )
+{
+    typedef typename SFCGAL::detail::HandleCollection<Dim>::Type HandleCollection;
+    typedef typename SFCGAL::detail::BoxCollection<Dim>::Type BoxCollection;
+    HandleCollection ahandles, bhandles;
+    BoxCollection aboxes, bboxes;
+    a.computeBoundingBoxes( ahandles, aboxes );
+    b.computeBoundingBoxes( bhandles, bboxes );
+
+    // if a is valid, then there shoudn't be any self intersection
+   
+    // note: it may be more efficient to union geometries that are disjoined first
+    // 
+    // now things are a bit tricky because several elements from a
+    // can combine with an element from b and yield one element
+    // what we need to do is 
+    // foreach b_elem in b
+    //          collect all element from b that intersect b_elem (bbox)
+    //          remove them from a
+    //          remove b_elem from b
+    //          peform the union of b_elem and all those elements
+    //          add the result to a 
+    // 
+    //
+    // add all remaining elements in b to a
+    
+    typename BoxCollection::iterator b_elem = bboxes.begin();
+    while ( b_elem != bboxes.end() ){
+        BoxCollection aboxes_intersecting_bboxes;
+        typename BoxCollection::iterator a_elem = aboxes.begin();
+        while ( a_elem != aboxes.end() ){
+            if ( CGAL::do_overlap(a_elem->bbox() , b_elem->bbox()) ){
+                aboxes_intersecting_bboxes.push_back( *a_elem );
+                a_elem = aboxes.erase( a_elem );
+            }
+            else {
+                ++a_elem;
+            }
+        }
+
+        if ( !aboxes_intersecting_bboxes.empty() ){
+            GeometrySet<Dim> result;
+
+            // TODO compute result
+
+            HandleCollection result_handles;
+            BoxCollection result_boxes;
+            result.computeBoundingBoxes( result_handles, result_boxes);
+            aboxes.insert( aboxes.end(), result_boxes.begin(), result_boxes.end());
+            b_elem = bboxes.erase( b_elem );
+        }
+        else {
+            ++b_elem;
+        }
+    }
+
+    aboxes.insert( aboxes.end(), bboxes.begin(), bboxes.end());
+
+    GeometrySet<Dim> temp, temp2;
+    typename BoxCollection::iterator a_elem = aboxes.begin();
+    const typename BoxCollection::iterator end = aboxes.end();
+    for (; a_elem != end; ++a_elem) temp.addPrimitive( *a_elem->handle() );
+
+    
+
+    // for intersecting primitive pairs (a_i, b_j), perform the union, repl
+    //{
+    //    dispatch_union_sym<Dim>( *a.handle(), *b.handle(), temp );
+    //}
+
+    post_union( temp, temp2 );
+    output.merge( temp2 );
+}
+
+template void union_<2>( const GeometrySet<2>& a, const GeometrySet<2>& b, GeometrySet<2>& );
+template void union_<3>( const GeometrySet<3>& a, const GeometrySet<3>& b, GeometrySet<3>& );
+
+std::auto_ptr<Geometry> union_( const Geometry& ga, const Geometry& gb, NoValidityCheck )
+{
+    GeometrySet<2> gsa( ga ), gsb( gb ), output;
+    algorithm::union_( gsa, gsb, output );
+
+    GeometrySet<2> filtered;
+    output.filterCovered( filtered );
+    return filtered.recompose();
+}
+
+std::auto_ptr<Geometry> union_( const Geometry& ga, const Geometry& gb )
+{
+    SFCGAL_ASSERT_GEOMETRY_VALIDITY_2D( ga );
+    SFCGAL_ASSERT_GEOMETRY_VALIDITY_2D( gb );
+
+    return union_( ga, gb, NoValidityCheck() );
+}
+
+std::auto_ptr<Geometry> union3D( const Geometry& ga, const Geometry& gb, NoValidityCheck )
+{
+    GeometrySet<3> gsa( ga ), gsb( gb ), output;
+    algorithm::union_( gsa, gsb, output );
+
+    GeometrySet<3> filtered;
+    output.filterCovered( filtered );
+
+    return filtered.recompose();
+}
+
+std::auto_ptr<Geometry> union3D( const Geometry& ga, const Geometry& gb )
+{
+    SFCGAL_ASSERT_GEOMETRY_VALIDITY_3D( ga );
+    SFCGAL_ASSERT_GEOMETRY_VALIDITY_3D( gb );
+
+    return union3D( ga, gb, NoValidityCheck() );
+}
+}
+}
