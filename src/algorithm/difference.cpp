346150f69b2a13ad16375076f16686f08ad981a0
vmora <vincent.mora@oslandia.com>
2014-10-24 12:23:03 +0200

    difference implementation (imcomplete)

--- src/algorithm/difference.cpp
+++ src/algorithm/difference.cpp
@@ -45,13 +45,290 @@ typedef CGAL::Triangle_2<Kernel> Triangle_2;
 
 namespace algorithm {
 
+template < typename K, typename OutputIterator>
+OutputIterator difference( const CGAL::Segment_2<K> & a, const CGAL::Segment_2<K> & b, OutputIterator out)
+{
+    CGAL::Object inter = CGAL::intersection( a, b );
+    const CGAL::Segment_2<K>* s = CGAL::object_cast< CGAL::Segment_2<K> >(&inter);
+    if ( s ){ // there maybe zero, one or two segments as a result
+        if (CGAL::squared_distance( a.source(), s->source() ) < CGAL::squared_distance( a.source(), s->target() )){
+            if ( a.source() != s->source() ) *out++ = CGAL::Segment_2<K>( a.source(), s->source() );
+            if ( s->target() != a.target() ) *out++ = CGAL::Segment_2<K>( s->target(), a.target() );
+        }
+        else{
+            if ( a.source() != s->target() ) *out++ = CGAL::Segment_2<K>( a.source(), s->target() );
+            if ( s->source() != a.target() ) *out++ = CGAL::Segment_2<K>( s->source(), a.target() );
+        }
+
+    }
+    else { // intersection is a point or nothing, leave a unchanged
+        *out++ = a;
+    }
+    return out;
+}
+    
+bool do_intersect( const CGAL::Point_2<Kernel>& point, const CGAL::Polygon_with_holes_2<Kernel> & polygon )
+{
+    // point intersects if it's inside the ext ring and outside all holes
+
+    if ( CGAL::bounded_side_2(polygon.outer_boundary().vertices_begin(), 
+                polygon.outer_boundary().vertices_end(), point, Kernel() ) 
+            ==  CGAL::ON_UNBOUNDED_SIDE) return false;
+
+    for ( CGAL::Polygon_with_holes_2<Kernel>::Hole_const_iterator hit = polygon.holes_begin();
+            hit != polygon.holes_end();
+            ++hit ) {
+        if ( CGAL::bounded_side_2(hit->vertices_begin(), 
+                    hit->vertices_end(), point, Kernel() ) 
+                !=  CGAL::ON_UNBOUNDED_SIDE) return false;
+    }
+
+    return true;
+}
+
+std::vector< CGAL::Point_2<Kernel> > difference( const CGAL::Point_2<Kernel> & primitive, const PrimitiveHandle<2>& pb )
+{
+    std::vector< CGAL::Point_2<Kernel> > result;
+    switch ( pb.handle.which() ){
+    case PrimitivePoint:
+        if ( primitive != *pb.as< CGAL::Point_2<Kernel> >() ) 
+            result.push_back( primitive );
+        break;
+    case PrimitiveSegment:
+        if ( ! CGAL::do_intersect( primitive, *pb.as< CGAL::Segment_2<Kernel> >() ) ) 
+            result.push_back( primitive );
+        break;
+    case PrimitiveSurface:
+        if ( ! do_intersect( primitive, *pb.as< CGAL::Polygon_with_holes_2<Kernel> >()  ) )
+            result.push_back( primitive );
+        break;
+    }
+    return result;
+}
+
+std::vector< CGAL::Segment_2<Kernel> > difference( const CGAL::Segment_2<Kernel> & primitive, const PrimitiveHandle<2>& pb )
+{
+    std::vector< CGAL::Segment_2<Kernel> > result;
+    switch ( pb.handle.which() ){
+    case PrimitivePoint:
+        result.push_back( primitive );
+        break;
+    case PrimitiveSegment:
+        difference( primitive,
+                *pb.as< CGAL::Segment_2<Kernel> >(), 
+                std::back_insert_iterator< std::vector< CGAL::Segment_2<Kernel> > >(result));
+        break;
+    case PrimitiveSurface:
+        BOOST_ASSERT(false);
+        break;
+    }
+    return result;
+}
+
+std::vector< CGAL::Polygon_with_holes_2<Kernel> > difference( const CGAL::Polygon_with_holes_2<Kernel> & primitive, const PrimitiveHandle<2>& pb )
+{
+    std::vector< CGAL::Polygon_with_holes_2<Kernel> > result;
+    switch ( pb.handle.which() ){
+    case PrimitivePoint:
+        result.push_back( primitive );
+        break;
+    case PrimitiveSegment:
+        result.push_back( primitive );
+        break;
+    case PrimitiveSurface:
+        CGAL::difference( primitive,
+                *pb.as< CGAL::Polygon_with_holes_2<Kernel> >(), 
+                std::back_insert_iterator< std::vector< CGAL::Polygon_with_holes_2<Kernel> > >(result));
+        break;
+    }
+    return result;
+}
+
+std::vector< CGAL::Point_3<Kernel> > difference( const CGAL::Point_3<Kernel> & primitive, const PrimitiveHandle<3>& pb )
+{
+    std::vector< CGAL::Point_3<Kernel> > result;
+    switch ( pb.handle.which() ){
+    case PrimitivePoint:
+        if ( primitive != *pb.as< CGAL::Point_3<Kernel> >() ) 
+            result.push_back( primitive );
+        break;
+    case PrimitiveSegment:
+        //if ( ! CGAL::do_intersect( primitive, *pb.as< CGAL::Segment_3<Kernel> >() ) ) 
+        //    result.push_back( primitive );
+
+        BOOST_ASSERT(false);
+        break;
+    case PrimitiveSurface:
+        BOOST_ASSERT(false);
+        break;
+    case PrimitiveVolume:
+        BOOST_ASSERT(false);
+        break;
+    }
+    return result;
+}
+std::vector< CGAL::Segment_3<Kernel> > difference( const CGAL::Segment_3<Kernel> & primitive, const PrimitiveHandle<3>& pb )
+{
+    std::vector< CGAL::Segment_3<Kernel> > result;
+    switch ( pb.handle.which() ){
+    case PrimitivePoint:
+        result.push_back( primitive );
+        break;
+    case PrimitiveSegment:
+        BOOST_ASSERT(false);
+        break;
+    case PrimitiveSurface:
+        BOOST_ASSERT(false);
+        break;
+    case PrimitiveVolume:
+        BOOST_ASSERT(false);
+        break;
+    }
+    return result;
+}
+
+std::vector< CGAL::Triangle_3<Kernel> > difference( const CGAL::Triangle_3<Kernel> & primitive, const PrimitiveHandle<3>& pb )
+{
+    std::vector< CGAL::Triangle_3<Kernel> > result;
+    switch ( pb.handle.which() ){
+    case PrimitivePoint:
+        result.push_back( primitive );
+        break;
+    case PrimitiveSegment:
+        result.push_back( primitive );
+        break;
+    case PrimitiveSurface:
+        BOOST_ASSERT(false);
+        break;
+    case PrimitiveVolume:
+        BOOST_ASSERT(false);
+        break;
+    }
+    return result;
+}
+
+std::vector< MarkedPolyhedron > difference( const MarkedPolyhedron & primitive, const PrimitiveHandle<3>& pb )
+{
+    std::vector< MarkedPolyhedron > result;
+    switch ( pb.handle.which() ){
+    case PrimitivePoint:
+        result.push_back( primitive );
+        break;
+    case PrimitiveSegment:
+        result.push_back( primitive );
+        break;
+    case PrimitiveSurface:
+        result.push_back( primitive );
+        break;
+    case PrimitiveVolume:
+        BOOST_ASSERT(false);
+        break;
+    }
+    return result;
+}
+
+
+template <typename Primitive, typename PrimitiveHandleConstIterator>
+std::vector< Primitive >
+difference( const Primitive & primitive, PrimitiveHandleConstIterator begin, PrimitiveHandleConstIterator end )
+{
+    std::vector< Primitive > primitives; 
+    primitives.push_back( primitive );
+    for (PrimitiveHandleConstIterator b = begin; b != end; ++b){
+        std::vector< Primitive > new_primitives; 
+        for ( typename std::vector< Primitive >::const_iterator a = primitives.begin(); 
+           a != primitives.end(); ++a ){
+            std::vector< Primitive > res( difference( *a, *(*b) ) );
+            new_primitives.insert( new_primitives.end(), res.begin(), res.end() );
+        }
+        primitives.swap(new_primitives);
+    }
+    return primitives;
+}
+
 template <int Dim>
 struct difference_cb {
+    typedef std::vector< PrimitiveHandle<Dim>* > PrimitiveHandleSet;
+    typedef std::map< PrimitiveHandle<Dim>*, PrimitiveHandleSet > Map;
+    difference_cb( Map & map ) : _map(map) {};
     void operator()( const typename PrimitiveBox<Dim>::Type& a,
                      const typename PrimitiveBox<Dim>::Type& b ) {
-        //*a.handle() -= *b.handle();
+        _map[a.handle()].push_back( b.handle() );
     }
+
+private:
+    Map & _map;
 };
+
+
+// just performs the type switch for the primitive to substract from
+//
+void appendDifference( const PrimitiveHandle<2>& pa, 
+        difference_cb<2>::PrimitiveHandleSet::const_iterator begin, 
+        difference_cb<2>::PrimitiveHandleSet::const_iterator end, 
+        GeometrySet<2>& output)
+{
+    switch ( pa.handle.which() ){
+    case PrimitivePoint:
+        {
+        std::vector< CGAL::Point_2<Kernel> > res = difference( 
+                *pa.as< CGAL::Point_2<Kernel> >(), begin, end );
+        output.addPoints( res.begin(), res.end() );
+        return;
+        }
+    case PrimitiveSegment:
+        {
+        std::vector< CGAL::Segment_2<Kernel> > res = difference( 
+                *pa.as< CGAL::Segment_2<Kernel> >(), begin, end );
+        output.addSegments( res.begin(), res.end() );
+        return;
+        }
+    case PrimitiveSurface:
+        {
+        std::vector< CGAL::Polygon_with_holes_2<Kernel> > res = difference( 
+                *pa.as< CGAL::Polygon_with_holes_2<Kernel> >(), begin, end );
+        output.addSurfaces( res.begin(), res.end() );
+        return;
+        }
+    }
+}
+
+void appendDifference( const PrimitiveHandle<3>& pa, 
+        difference_cb<3>::PrimitiveHandleSet::const_iterator begin, 
+        difference_cb<3>::PrimitiveHandleSet::const_iterator end, 
+        GeometrySet<3>& output)
+{
+    switch ( pa.handle.which() ){
+    case PrimitivePoint:
+        {
+        std::vector< CGAL::Point_3<Kernel> > res = difference( 
+                *pa.as< CGAL::Point_3<Kernel> >(), begin, end );
+        output.addPoints( res.begin(), res.end() );
+        return;
+        }
+    case PrimitiveSegment:
+        {
+        std::vector< CGAL::Segment_3<Kernel> > res = difference( 
+                *pa.as< CGAL::Segment_3<Kernel> >(), begin, end );
+        output.addSegments( res.begin(), res.end() );
+        break;
+        }
+    case PrimitiveSurface:
+        {
+        std::vector< CGAL::Triangle_3<Kernel> > res = difference( 
+                *pa.as< CGAL::Triangle_3<Kernel> >(), begin, end );
+        output.addSurfaces( res.begin(), res.end() );
+        break;
+        }
+    case PrimitiveVolume:
+        {
+        std::vector< MarkedPolyhedron > res = difference( 
+                *pa.as< MarkedPolyhedron >(), begin, end );
+        output.addVolumes( res.begin(), res.end() );
+        break;
+        }
+    }
+}
 
 /**
  * difference post processing
@@ -99,17 +376,43 @@ void post_difference( const GeometrySet<3>& input, GeometrySet<3>& output )
 template <int Dim>
 void difference( const GeometrySet<Dim>& a, const GeometrySet<Dim>& b, GeometrySet<Dim>& output )
 {
+    typedef typename SFCGAL::detail::BoxCollection<Dim>::Type BoxCollection;
     typename SFCGAL::detail::HandleCollection<Dim>::Type ahandles, bhandles;
-    typename SFCGAL::detail::BoxCollection<Dim>::Type aboxes, bboxes;
+    BoxCollection aboxes, bboxes;
     a.computeBoundingBoxes( ahandles, aboxes );
     b.computeBoundingBoxes( bhandles, bboxes );
 
+    // here we use box_intersection_d to build the list of operations
+    // that actually need to be performed
     GeometrySet<Dim> temp, temp2;
-    difference_cb<Dim> cb;
+    typename difference_cb<Dim>::Map map;
+    difference_cb<Dim> cb(map);
     CGAL::box_intersection_d( aboxes.begin(), aboxes.end(),
                               bboxes.begin(), bboxes.end(),
                               cb );
 
+    // now we have in cb a map of operations to perform
+    // we can put in the result right away all the primitives
+    // that are not keys in this map
+    {
+        typename BoxCollection::const_iterator ait = aboxes.begin();
+        const typename BoxCollection::const_iterator end = aboxes.end();
+        for ( ; ait != end; ++ait ){
+            if ( map.find( ait->handle() ) == map.end() ) {
+                temp.addPrimitive( *ait->handle() );
+            }
+        }
+    }
+
+    // then we delegate the operations according to type
+    {
+        typename difference_cb<Dim>::Map::const_iterator cbit = map.begin();
+        const typename difference_cb<Dim>::Map::const_iterator end = map.end();
+        for ( ; cbit != end; ++cbit ){
+            appendDifference( *cbit->first, cbit->second.begin(), cbit->second.end(), temp);
+        }
+    }
+    
     post_difference( temp, temp2 );
     output.merge( temp2 );
 }
