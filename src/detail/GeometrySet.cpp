46686fc830f94f672de737d11f4fa7f73e3b88c6
Hugo Mercier <hugo.mercier@oslandia.com>
2015-04-08 11:09:57 +0200

    Fix a compilation error on Mac

--- src/detail/GeometrySet.cpp
+++ src/detail/GeometrySet.cpp
@@ -557,10 +557,10 @@ void recompose_points( const typename GeometrySet<Dim>::PointCollection& points,
 
 // compare less than
 struct ComparePoints {
-    bool operator()( const CGAL::Point_2<Kernel>& lhs, const CGAL::Point_2<Kernel>& rhs ) {
+    bool operator()( const CGAL::Point_2<Kernel>& lhs, const CGAL::Point_2<Kernel>& rhs ) const {
         return lhs.x() < rhs.x() || lhs.y() < rhs.y();
     }
-    bool operator()( const CGAL::Point_3<Kernel>& lhs, const CGAL::Point_3<Kernel>& rhs ) {
+    bool operator()( const CGAL::Point_3<Kernel>& lhs, const CGAL::Point_3<Kernel>& rhs ) const {
         return lhs.x() < rhs.x() || lhs.y() < rhs.y() || lhs.z() < rhs.z();
     }
 };
