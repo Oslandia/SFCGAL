5093e949599f7fb6a7045df3a0a6324d1dd055d5
Hugo Mercier <hugo.mercier@oslandia.com>
2015-11-10 11:34:03 +0100

    Cmake 2.8.11 for travis

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -1,11 +1,13 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo add-apt-repository ppa:apokluda/boost1.53 --yes
+sudo add-apt-repository ppa:kalakris/cmake --yes # CMAKE 2.8.11
 sudo apt-get update -qq
 sudo apt-get install --force-yes \
     cmake libboost-chrono1.53-dev libboost-program-options1.53-dev libboost-filesystem1.53-dev libboost-timer1.53-dev \
     libboost-test1.53-dev libboost-date-time1.53-dev libboost-thread1.53-dev \
     libboost-system1.53-dev libboost-serialization1.53-dev \
-    libmpfr-dev libgmp-dev
+    libmpfr-dev libgmp-dev \
+    cmake
 #CGAL
 if [ "$1" = "4.3" ]; then
     wget https://gforge.inria.fr/frs/download.php/file/32994/CGAL-4.3.tar.gz
