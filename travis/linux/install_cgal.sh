86a0d5bd89e25f7871e896f44a1d63429287358c
Hugo Mercier <hugo.mercier@oslandia.com>
2015-11-10 10:50:42 +0100

    Add CGAL 4.7 to Travis

---
+++ travis/linux/install_cgal.sh
@@ -0,0 +1,14 @@
+#CGAL
+if [ "$1" = "4.3" ]; then
+    wget https://gforge.inria.fr/frs/download.php/file/32994/CGAL-4.3.tar.gz
+    tar xzf CGAL-4.3.tar.gz
+    cd CGAL-4.3 && cmake -DCMAKE_INSTALL_PREFIX=$HOME/CGAL-4.3 . && make && make install && cd ..
+elif [ "$1" = "4.7" ]; then
+    wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz
+    tar xzf CGAL-4.7.tar.gz
+    cd CGAL-4.7 && cmake -DCMAKE_INSTALL_PREFIX=$HOME/CGAL-4.7 . && make && make install && cd ..
+fi
+
+
+cmake --version
+clang --version
