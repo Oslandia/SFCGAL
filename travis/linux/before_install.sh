a70834a9bd67c55b6820013d27a9a008577a8fe7
Hugo Mercier <hugo.mercier@oslandia.com>
2015-11-10 10:50:42 +0100

    Test

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -7,9 +7,15 @@ sudo apt-get install --force-yes \
     libboost-system1.53-dev libboost-serialization1.53-dev \
     libmpfr-dev libgmp-dev
 #CGAL
-wget https://gforge.inria.fr/frs/download.php/file/32994/CGAL-4.3.tar.gz
-tar xzf CGAL-4.3.tar.gz
-cd CGAL-4.3 && cmake . && make && sudo make install && cd ..
+if [ "$1" = "4.3" ]; then
+    wget https://gforge.inria.fr/frs/download.php/file/32994/CGAL-4.3.tar.gz
+    tar xzf CGAL-4.3.tar.gz
+    cd CGAL-4.3 && cmake . && make && sudo make install && cd ..
+elif [ "$1" = "4.7" ]; then
+    wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.7/CGAL-4.7.tar.gz
+    tar xzf CGAL-4.7.tar.gz
+    cd CGAL-4.7 && cmake . && make && sudo make install && cd ..
+fi
 
 
 cmake --version
