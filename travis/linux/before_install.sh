147c939de66fb544ac8350f9035f54af10a98637
Hugo Mercier <hugo.mercier@oslandia.com>
2018-11-12 13:35:10 +0100

    Upgrade Travis to CGAL 4.13 and Ubuntu xenial

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -1,17 +1,16 @@
 export DEBIAN_FRONTEND=noninteractive
-sudo -E apt-add-repository -y "ppa:ppsspp/cmake"
 sudo apt-get update -qq
 sudo apt-get install --force-yes \
-    cmake libboost-chrono1.55-dev libboost-program-options1.55-dev libboost-filesystem1.55-dev libboost-timer1.55-dev \
-    libboost-test1.55-dev libboost-date-time1.55-dev libboost-thread1.55-dev \
-    libboost-system1.55-dev libboost-serialization1.55-dev \
+    cmake libboost-chrono1.58-dev libboost-program-options1.58-dev libboost-filesystem1.58-dev libboost-timer1.58-dev \
+    libboost-test1.58-dev libboost-thread1.58-dev \
+    libboost-system1.58-dev libboost-serialization1.58-dev \
     libmpfr-dev libgmp-dev \
     cmake
 #CGAL
 
-wget https://github.com/CGAL/cgal/archive/releases/CGAL-"$1".tar.gz
-tar xzf CGAL-"$1".tar.gz
-cd cgal-releases-CGAL-"$1" && cmake . && make && sudo make install && cd ..
+wget https://github.com/CGAL/cgal/releases/download/releases/CGAL-"$1"/CGAL-"$1".tar.xz
+tar xJf CGAL-"$1".tar.xz
+cd CGAL-"$1" && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$HOME/CGAL-"$1" .. && make && make install && cd ../..
 
 cmake --version
 clang --version
--- travis/linux/install_cgal.sh
+++
@@ -1,8 +0,0 @@
-#CGAL
-
-wget https://github.com/CGAL/cgal/archive/releases/CGAL-"$1".tar.gz
-tar xzf CGAL-"$1".tar.gz
-cd CGAL-"$1" && cmake -DCMAKE_INSTALL_PREFIX=$HOME/CGAL-"$1" . && make && make install && cd ..
-
-cmake --version
-clang --version
