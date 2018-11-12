9144c3a7acfa5bc131d3d5b290ffdbd1351d56fa
Hugo Mercier <hugo.mercier@oslandia.com>
2018-11-12 12:05:42 +0100

    sfldksmdlfk

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -8,7 +8,7 @@ sudo apt-get install --force-yes \
     cmake
 #CGAL
 
-wget https://github.com/CGAL/cgal/releases/download/releases-CGAL-"$1"/CGAL-"$1".tar.xz
+wget https://github.com/CGAL/cgal/releases/download/releases/CGAL-"$1"/CGAL-"$1".tar.xz
 tar xJf CGAL-"$1".tar.xz
 cd CGAL-"$1" && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$HOME/CGAL-"$1" .. && make && make install && cd ../..
 
