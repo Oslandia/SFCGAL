4fd25a2e1385ace18ec9d0c2c864a530ffa0ce6c
Hugo Mercier <hugo.mercier@oslandia.com>
2018-11-12 11:57:20 +0100

    sdfsdfsdf

--- travis/linux/install_cgal.sh
+++ travis/linux/install_cgal.sh
@@ -1,7 +1,7 @@
 #CGAL
 
-wget https://github.com/CGAL/cgal/archive/releases/CGAL-"$1".tar.gz
-tar xzf CGAL-"$1".tar.gz
+wget https://github.com/CGAL/cgal/releases/download/releases-CGAL-"$1"/CGAL-"$1".tar.xz
+tar xJf CGAL-"$1".tar.xz
 cd CGAL-"$1" && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$HOME/CGAL-"$1" .. && make && make install && cd ../..
 
 clang --version
