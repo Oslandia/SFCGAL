2f33da2ec9352d139f28fcaaa86b23c4c950110a
Hugo Mercier <hugo.mercier@oslandia.com>
2018-11-12 11:38:23 +0100

    testjsdfg

--- travis/linux/install_cgal.sh
+++ travis/linux/install_cgal.sh
@@ -2,7 +2,7 @@
 
 wget https://github.com/CGAL/cgal/archive/releases/CGAL-"$1".tar.gz
 tar xzf CGAL-"$1".tar.gz
-cd CGAL-"$1" && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$HOME/CGAL-"$1" . && make && make install && cd ../..
+cd CGAL-"$1" && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$HOME/CGAL-"$1" .. && make && make install && cd ../..
 
 cmake --version
 clang --version
